5d528d1cad90b319886217a6ce135e60561c1a8c
LeonIAGT <leon.alvarez@iagt.es>
2018-06-07 20:35:37 +0200

    update config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
-  siteTitle: "Weird JS", // Site title.
-  siteTitleAlt: "Weird JS gotchasr", // Alternative site title for SEO.
+  blogPostDir: "snippets", // The name of directory that contains your posts.
+  siteTitle: "Coding Snippets", // Site title.
+  siteTitleAlt: "Coding Snippets", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "http://weird.jsdev.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/weirdJS", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Weird JS gotchas. Don't get caught", // Website description used for RSS feeds/meta description tag.
+  siteUrl: "https://snippets.kodeloper.io/", // Domain of your website without pathPrefix.
+  pathPrefix: "/snippets", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "Reusable conding snippets", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
@@ -13,7 +13,7 @@ module.exports = {
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "Advanced User", // Username to display in the author segment.
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  userLocation: "Sevilla, Spain", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
@@ -31,11 +31,11 @@ module.exports = {
     },
     {
       label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      url: "mailto:leon@leon-design.es",
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2018. Weird JS", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2018. Kodeloper", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0" // Used for setting manifest background color.
 };
