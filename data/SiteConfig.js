e851325cf801d6cc79645401d7d3dab403e64b76
LeonIAGT <leon.alvarez@iagt.es>
2018-06-09 18:23:24 +0200

    ga

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,11 +4,11 @@ module.exports = {
   siteTitleAlt: "Coding Snippets", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://snippets.kodeloper.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/snippets", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Reusable conding snippets", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+  googleAnalyticsID: "UA-120607950-1", // GA tracking ID.
   disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "Advanced User", // Username to display in the author segment.
