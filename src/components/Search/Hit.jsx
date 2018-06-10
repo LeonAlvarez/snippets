21b5312584fb3003c3ad0f218846303218336700
LeonIAGT <leon.alvarez@iagt.es>
2018-06-10 12:56:23 +0200

    update

--- src/components/Search/Hit.jsx
+++ src/components/Search/Hit.jsx
@@ -4,14 +4,8 @@ import lodash from "lodash";
 import {LayoutContext} from "../../layouts/layout-context";
 
 class Hit extends React.Component {
-  constructor(props){
-    super(props);
-    console.log(this.props.toggleSearch)
-    this.toggleSearch = this.props.toggleSearch;
-  }
   render() {
     const {hit} = this.props;
-    console.log(this.props);
     return (
       <LayoutContext.Consumer>
         {({toggleSearch}) => (
